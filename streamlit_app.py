c8bbdafcce6b15523ab51316ac516fe3063e26e9
Alvaro Sainz-Pardo <alvarosainzpardo@gmail.com>
2025-11-20 12:01:27 +0100

    update

---
+++ streamlit_app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.markdown("""
+    # My first Streamlit app
+
+    This is my first and simple Streamlit app
+
+    - One list element
+    - Another list element
+
+    Bye!
+""")
